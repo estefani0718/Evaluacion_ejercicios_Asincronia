4f24828b7fcbd69f220e0fcb36e0ac9c5e90a585
estefani0718 <eestefanisg@gmail.com>
2025-03-19 00:12:26 -0500

    comentarios

--- MODULOS/Usuarios/usuarios_tareas_pendientes.js
+++ MODULOS/Usuarios/usuarios_tareas_pendientes.js
@@ -1,10 +1,11 @@
 import { solicitud } from "../Request/solicitud_todos";
-
-
+// importamos la funcion que reutiizaremos el codigo que esta en este modulo o carpeta
+//exportamos la funcion 
+// esta funcion almacena la respuesta de todos los usuarios 
 export const listarTareasUsuario = async (URL) => {
     const tareas = await solicitud(`${URL}/todos`);
-    const data = await tareas.json();
-    return data;
+    const data = await tareas.json();// se convierte en algo mas entendible en este caso un json , donde estaran los datos que solictamos anteriormente 
+    return data;// se retorna los datos esperados 
 }
   
   
--- MODULOS/index.js
+++ MODULOS/index.js
@@ -3,3 +3,5 @@ export * as Albumes from "./Albumes/index.js";
 export * as Fotos from "./Fotos/index.js";
 export * as Posts from "./Posts/index.js";
 export * as Coments from "./Coments/index.js";
+// se exporta todo el contenido del modulo y se le asigna un nombre osea un alias para cada exportacion 
+// asi la vamos allmar en el archivo barril principal 
